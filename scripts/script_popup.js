5c99a17bbd2ef3e6a917105eabb312881d1d327a
Yulia Kremneva <yulia.nsk@gmail.com>
2020-03-18 00:31:17 +0700

    correct bugs

--- scripts/script_popup.js
+++ scripts/script_popup.js
@@ -80,3 +80,14 @@ $('.popup__container1').click(function(event) {
     });
 }));
 });
+
+Share = {
+  me : function(el){
+      Share.popup(el.href);
+      return false;
+  },
+
+  popup: function(url) {
+      window.open(url,'','toolbar=0,status=0,width=626,height=436');
+  }
+};
